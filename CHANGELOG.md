4a7a995b3fad8b2e5bc79dfb5a5b5f95e27876e5
Romain Ruetschi <romain@informal.systems>
2023-10-25 17:22:13 +0200

    Add support for new `timestamp` field in meta section of ITF traces

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # CHANGELOG
 
+## Unreleased
+
+- Add support for new `timestamp` field in meta section of ITF traces
+
 ## v0.1
 
 *March 24th, 2023*
--- itf/src/meta.rs
+++ itf/src/meta.rs
@@ -5,19 +5,22 @@ use serde::{Deserialize, Serialize};
 #[derive(Default, Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct TraceMeta {
     #[serde(default)]
-    pub description: Option<String>,
+    pub format: Option<String>,
+
+    #[serde(rename = "format-description")]
+    pub format_description: Option<String>,
 
     #[serde(default)]
     pub source: Option<String>,
+
+    #[serde(default)]
+    pub description: Option<String>,
 
     #[serde(default, rename = "varTypes")]
     pub var_types: HashMap<String, String>,
 
     #[serde(default)]
-    pub format: Option<String>,
-
-    #[serde(rename = "format-description")]
-    pub format_description: Option<String>,
+    pub timestamp: Option<u64>,
 
     #[serde(flatten)]
     pub other: HashMap<String, String>,
