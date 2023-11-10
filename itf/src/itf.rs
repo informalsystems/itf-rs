07e8106c9c4cadf0bda20e9333cf665412cb5aa3
Romain Ruetschi <romain@informal.systems>
2023-11-10 15:06:29 +0100

    Add `From<T> where T: From<BigInt>` instance for `ItfBigInt`

--- itf/src/itf.rs
+++ itf/src/itf.rs
@@ -171,6 +171,15 @@ deserialize_itf_tuple!(7,  0 A 1 B 2 C 3 D 4 E 5 F 6 G);
 // deserialize_itf_tuple!(11, 0 A 1 B 2 C 3 D 4 E 5 F 6 G 7 H 8 I 9 J 10 K);
 // deserialize_itf_tuple!(12, 0 A 1 B 2 C 3 D 4 E 5 F 6 G 7 H 8 I 9 J 10 K 11 L);
 
+impl<T> From<T> for ItfBigInt
+where
+    BigInt: From<T>,
+{
+    fn from(t: T) -> Self {
+        Itf(BigInt::from(t))
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
