e39f14168a5fe069dfc47b33a6c7a5619155438b
Romain Ruetschi <romain@informal.systems>
2023-11-21 14:50:47 +0100

    Remove unused `Error::Io` variant

--- itf/src/error.rs
+++ itf/src/error.rs
@@ -1,14 +1,8 @@
+/// Error type for the library.
 #[derive(Debug)]
 pub enum Error {
-    Io(std::io::Error),
     Json(serde_json::Error),
     Decode(crate::de::Error),
-}
-
-impl From<std::io::Error> for Error {
-    fn from(v: std::io::Error) -> Self {
-        Self::Io(v)
-    }
 }
 
 impl From<serde_json::Error> for Error {
@@ -26,7 +20,6 @@ impl From<crate::de::Error> for Error {
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
-            Error::Io(e) => write!(f, "I/O error: {}", e),
             Error::Json(e) => write!(f, "JSON error: {}", e),
             Error::Decode(e) => write!(f, "decoding error: {}", e),
         }
@@ -36,7 +29,6 @@ impl std::fmt::Display for Error {
 impl std::error::Error for Error {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
         match self {
-            Error::Io(e) => Some(e),
             Error::Json(e) => Some(e),
             Error::Decode(e) => Some(e),
         }
